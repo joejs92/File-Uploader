20ca657f8c5a2ef01975b4b34d763cebc852f461
joejs92 <joejs_92@yahoo.com>
2025-07-17 13:14:28 -0400

    made rough landing page

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const {Router} = require("express");
+/* const controller = require("../controllers/controller") */
+//const {home} = require("../views/index");
+
+const index = Router();
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+/* index.get("/", (req, res) => {
+  res.render("index", { links: links });
+}); */
+index.get("/", (req, res)=> res.render("index", {message: "Fuck you, World!"}));
+
+module.exports = index;
