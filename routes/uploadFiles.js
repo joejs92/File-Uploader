c411adbc141a932db5a6898b0db9eb61e8d16f3f
joejs92 <joejs_92@yahoo.com>
2025-07-26 23:20:52 -0400

    get file uploader to work locally

--- routes/uploadFiles.js
+++ routes/uploadFiles.js
@@ -1,10 +1,13 @@
 const {Router} = require("express");
-/* const controller = require("../controllers/controller") */
+const controller = require("../controllers/upload")
 
 const uploadFiles = Router();
 
 const title = "Upload Files";
 
 uploadFiles.get("/", (req, res)=> res.render("uploadFiles", { title: title, user: req.user }));
+
+uploadFiles.post("/", controller.single('file'),(req, res)=> res.render("uploadFiles", { title: title, user: req.user }));
+
 
 module.exports = uploadFiles;
