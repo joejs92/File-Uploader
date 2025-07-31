917705958158678eb5fb8b640e77d456a81d5cb2
joejs92 <joejs_92@yahoo.com>
2025-07-30 22:22:32 -0400

    get links in folderview working

--- routes/fileViewer.js
+++ routes/fileViewer.js
@@ -4,17 +4,18 @@ const controller = require("../controllers/controller")
 const fileViewer = Router();
 
 const links = [
-  { href: "profile", text: "Profile" },
+  { href: "/profile", text: "Profile" },
   { href: "logout", text: "Log Out" }
 ];
-
+//for some reason, the link to profile doesn't work without the slash in front
 const title = "File Viewer";
-/* 
-async function folderMiddleware(req, res, next){
-  req.folders = await controller.getFolders(req, res);
-  next();
-} */
 
-fileViewer.get("/", (req, res)=> res.render("fileViewer", { links: links, title: title, user: req.user}));
+async function folderMiddleware(req, res, next){
+  req.folder = await controller.getSpecificFolder(req, res);
+  console.log(req.folder);
+  next();
+}
+
+fileViewer.get("/:folderId", folderMiddleware, (req, res)=> res.render("fileViewer", { links: links, title: req.folder.foldername, user: req.user, folder: req.folder}));
 
 module.exports = fileViewer;
